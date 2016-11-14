2051421573d263dbcd1ac03032d5224a3b4c31cf
Kristin Sztengel <kcsztengel@gmail.com>
2016-11-14 15:32:51 -0700

    geocode key added

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,10 @@
 class Shop < ActiveRecord::Base
+
+
   geocoded_by :full_street_address
-  after_validation :geocode 
+  after_validation :geocode
+
+
+def full_street_address
+  [address, city, state,zip ].compact.join(', ')
 end
