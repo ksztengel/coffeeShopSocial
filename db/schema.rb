4c5b1f7f7e64148474bd05178646b071a6f8c882
Kristin Sztengel <kcsztengel@gmail.com>
2016-11-15 10:20:19 -0700

    added deals table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115165008) do
+ActiveRecord::Schema.define(version: 20161115170559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "deals", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "imgUrl"
+    t.integer  "shops_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "shop_users", force: :cascade do |t|
     t.string   "name"
