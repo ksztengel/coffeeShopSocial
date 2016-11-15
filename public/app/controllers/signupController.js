c9519cc4aeef698f47299e088c0eb393a7aae605
courtneysanders418 <cswoods88@gmail.com>
2016-11-15 16:10:32 -0700

    added signup controller and html and partials

---
+++ public/app/controllers/signupController.js
@@ -0,0 +1,13 @@
+app.controller('auth', function($scope) {
+  
+    $scope.userObj = {}
+    $scope.businessObj = {}
+
+    $scope.signup = function(object) {
+      console.log(object);
+    }
+    $scope.login = function (object) {
+      console.log(object);
+    }
+
+})
