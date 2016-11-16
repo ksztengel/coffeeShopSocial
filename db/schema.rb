0b8c8b583efa67e6ca260a815fbd8199b577615d
Matt Works <mworks4905@gmail.com>
2016-11-16 14:59:48 -0700

    added main text and hero image

--- db/schema.rb
+++ db/schema.rb
@@ -11,29 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115212405) do
+ActiveRecord::Schema.define(version: 20161114173138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "deals", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.string   "imgUrl"
-    t.integer  "shops_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "shop_users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_hash"
-    t.string   "password_salt"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.integer  "shops_id"
-  end
 
   create_table "shops", force: :cascade do |t|
     t.string   "name"
@@ -43,17 +24,6 @@ ActiveRecord::Schema.define(version: 20161115212405) do
     t.integer  "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float    "latitude"
-    t.float    "longitude"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_hash"
-    t.string   "password_salt"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
   end
 
 end
