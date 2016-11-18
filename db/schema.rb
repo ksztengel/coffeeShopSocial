9ed796d4ccbd80a70a991f94449f3506d1c3f832
courtneysanders418 <cswoods88@gmail.com>
2016-11-18 11:25:05 -0700

    fljsd

--- db/schema.rb
+++ db/schema.rb
@@ -47,4 +47,13 @@ ActiveRecord::Schema.define(version: 20161117190030) do
     t.float    "longitude"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
 end
